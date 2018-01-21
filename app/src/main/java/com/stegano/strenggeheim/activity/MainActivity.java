ff3e7bda2f779c810db8b6e243f23d762a55c615
Jai Prakash <jaiprak@thoughtworks.com>
2018-01-21 17:58:00 +0530

    Notification features and right menu is removed. | Jai

--- app/src/main/java/com/stegano/strenggeheim/activity/MainActivity.java
+++ app/src/main/java/com/stegano/strenggeheim/activity/MainActivity.java
@@ -17,12 +17,9 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
@@ -30,7 +27,6 @@ import com.stegano.strenggeheim.R;
 import com.stegano.strenggeheim.fragment.FragmentDecode;
 import com.stegano.strenggeheim.fragment.FragmentEncode;
 import com.stegano.strenggeheim.fragment.HomeFragment;
-import com.stegano.strenggeheim.fragment.NotificationsFragment;
 import com.stegano.strenggeheim.fragment.SettingsFragment;
 import com.stegano.strenggeheim.utils.stego.BitmapHelper;
 import com.stegano.strenggeheim.utils.stego.Steganographer;
@@ -53,19 +49,14 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.ic_tab_decode
     };
 
-    // index to identify current nav menu item
     public static int navItemIndex = 0;
 
-    // tags used to attach the fragments
     private static final String TAG_HOME = "home";
-    private static final String TAG_NOTIFICATIONS = "notifications";
     private static final String TAG_SETTINGS = "settings";
     public static String CURRENT_TAG = TAG_HOME;
 
-    // toolbar titles respected to selected nav menu item
     private String[] activityTitles;
 
-    // flag to load home fragment when user presses back key
     private boolean shouldLoadHomeFragOnBackPress = true;
     private Handler mHandler;
 
@@ -89,20 +80,16 @@ public class MainActivity extends AppCompatActivity {
         setupTab();
 
         mHandler = new Handler();
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
-        // Navigation view header
         navHeader = navigationView.getHeaderView(0);
-        imgProfile = (ImageView) navHeader.findViewById(R.id.img_profile);
+        imgProfile = navHeader.findViewById(R.id.img_profile);
 
-        // load toolbar titles from string resources
         activityTitles = getResources().getStringArray(R.array.nav_item_activity_titles);
 
-        // load nav menu header data
         loadNavHeader();
 
-        // initializing navigation menu
         setUpNavigationView();
 
         if (savedInstanceState == null) {
@@ -118,32 +105,21 @@ public class MainActivity extends AppCompatActivity {
                 .thumbnail(0.5f)
                 .apply(RequestOptions.circleCropTransform())
                 .into(imgProfile);
-
-        navigationView.getMenu().getItem(1).setActionView(R.layout.menu_dot);
     }
 
     private void loadHomeFragment() {
-        // selecting appropriate nav menu item
         selectNavMenu();
 
-        // set toolbar title
         setToolbarTitle();
 
-        // if user select the current navigation menu again, don't do anything
-        // just close the navigation drawer
         if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
             drawer.closeDrawers();
             return;
         }
 
-        // Sometimes, when fragment has huge data, screen seems hanging
-        // when switching between navigation menus
-        // So using runnable, the fragment is loaded with cross fade effect
-        // This effect can be seen in GMail app
         Runnable mPendingRunnable = new Runnable() {
             @Override
             public void run() {
-                // update the main content by replacing fragments
                 Fragment fragment = getHomeFragment();
                 FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
                 fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
@@ -153,15 +129,12 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        // If mPendingRunnable is not null, then add to the message queue
         if (mPendingRunnable != null) {
             mHandler.post(mPendingRunnable);
         }
 
-        //Closing drawer on item click
         drawer.closeDrawers();
 
-        // refresh toolbar menu
         invalidateOptionsMenu();
     }
 
@@ -172,12 +145,8 @@ public class MainActivity extends AppCompatActivity {
                 HomeFragment homeFragment = new HomeFragment();
                 return homeFragment;
                 //viewPager.setCurrentItem(0, false);
-            case 1:
-                // notifications fragment
-                NotificationsFragment notificationsFragment = new NotificationsFragment();
-                return notificationsFragment;
 
-            case 2:
+            case 1:
                 // settings fragment
                 SettingsFragment settingsFragment = new SettingsFragment();
                 return settingsFragment;
@@ -195,30 +164,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setUpNavigationView() {
-        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
 
-            // This method will trigger on item Click of navigation menu
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
 
-                //Check to see which item was being clicked and perform appropriate action
                 switch (menuItem.getItemId()) {
-                    //Replacing the main content with ContentFragment Which is our Inbox View;
                     case R.id.nav_home:
                         navItemIndex = 0;
                         CURRENT_TAG = TAG_HOME;
                         break;
-                    case R.id.nav_notifications:
-                        navItemIndex = 1;
-                        CURRENT_TAG = TAG_NOTIFICATIONS;
-                        break;
                     case R.id.nav_settings:
-                        navItemIndex = 2;
+                        navItemIndex = 1;
                         CURRENT_TAG = TAG_SETTINGS;
                         break;
                     case R.id.nav_about_us:
-                        // launch new intent instead of loading fragment
                         startActivity(new Intent(MainActivity.this, AboutUsActivity.class));
                         drawer.closeDrawers();
                         return true;
@@ -226,7 +186,6 @@ public class MainActivity extends AppCompatActivity {
                         navItemIndex = 0;
                 }
 
-                //Checking if the item is in checked state or not, if not make it in checked state
                 if (menuItem.isChecked()) {
                     menuItem.setChecked(false);
                 } else {
@@ -243,21 +202,17 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onDrawerClosed(View drawerView) {
-                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
                 super.onDrawerClosed(drawerView);
             }
 
             @Override
             public void onDrawerOpened(View drawerView) {
-                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
                 super.onDrawerOpened(drawerView);
             }
         };
 
-        //Setting the actionbarToggle to drawer layout
         drawer.addDrawerListener(actionBarDrawerToggle);
 
-        //calling sync state is necessary or else your hamburger icon wont show up
         actionBarDrawerToggle.syncState();
     }
 
@@ -268,11 +223,7 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        // This code loads home fragment when back key is pressed
-        // when user is in other fragment than home
         if (shouldLoadHomeFragOnBackPress) {
-            // checking if user is on other navigation menu
-            // rather than home
             if (navItemIndex != 0) {
                 navItemIndex = 0;
                 CURRENT_TAG = TAG_HOME;
@@ -282,50 +233,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         super.onBackPressed();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-
-        // show menu only when home fragment is selected
-        if (navItemIndex == 0) {
-            getMenuInflater().inflate(R.menu.main, menu);
-        }
-
-        // when fragment is notifications, load the menu created for notifications
-        if (navItemIndex == 3) {
-            getMenuInflater().inflate(R.menu.notifications, menu);
-        }
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_logout) {
-            Toast.makeText(getApplicationContext(), "Logout user!", Toast.LENGTH_LONG).show();
-            return true;
-        }
-
-        // user is in notifications fragment
-        // and selected 'Mark all as Read'
-        if (id == R.id.action_mark_all_read) {
-            Toast.makeText(getApplicationContext(), "All notifications marked as read!", Toast.LENGTH_LONG).show();
-        }
-
-        // user is in notifications fragment
-        // and selected 'Clear All'
-        if (id == R.id.action_clear_notifications) {
-            Toast.makeText(getApplicationContext(), "Clear all notifications!", Toast.LENGTH_LONG).show();
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private void setupViewPager(ViewPager viewPager) {
@@ -375,7 +282,6 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override public void run() {
                 try {
-//                    Test.runTests();
                     attempEncoding();
                 } catch (Exception e) {
                     e.printStackTrace();
--- app/src/main/java/com/stegano/strenggeheim/fragment/NotificationsFragment.java
+++
@@ -1,6 +0,0 @@
-package com.stegano.strenggeheim.fragment;
-
-import android.support.v4.app.Fragment;
-
-public class NotificationsFragment extends Fragment {
-}
