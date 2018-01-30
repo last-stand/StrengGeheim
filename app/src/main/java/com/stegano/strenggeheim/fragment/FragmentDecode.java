a05a660f5a51a9c1a197d8fa162a5e5e03bd138c
Jai Prakash <jaiprak@thoughtworks.com>
2018-01-30 12:51:11 +0530

    Refactoring | Jai

--- app/src/main/java/com/stegano/strenggeheim/fragment/FragmentDecode.java
+++ app/src/main/java/com/stegano/strenggeheim/fragment/FragmentDecode.java
@@ -21,7 +21,6 @@ public class FragmentDecode extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_decode, container, false);
     }
 }
