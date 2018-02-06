0034041ef3ad706d818ccb12151a08920d22b050
Jai Prakash <jaiprak@thoughtworks.com>
2018-02-06 16:02:05 +0530

    Selecting Fragment Encode on Home option in drawer menu. | Jai

--- app/src/main/java/com/stegano/strenggeheim/fragment/HomeFragment.java
+++ app/src/main/java/com/stegano/strenggeheim/fragment/HomeFragment.java
@@ -1,6 +1,19 @@
 package com.stegano.strenggeheim.fragment;
 
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 
+import com.stegano.strenggeheim.R;
+
 public class HomeFragment extends Fragment {
+    private TabLayout tabLayout;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        tabLayout = getActivity().findViewById(R.id.tabs);
+        TabLayout.Tab tab = tabLayout.getTabAt(0);
+        tab.select();
+    }
 }
