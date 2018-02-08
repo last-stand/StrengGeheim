c422e4d8a138e00cb53184a7a65f70e090c09564
Jai Prakash <jaiprak@thoughtworks.com>
2018-02-08 11:32:51 +0530

    Big fix, while choosing option 'file explorer' to select image, it was showing empty 'dia' folder | Jai

--- app/src/main/java/com/stegano/strenggeheim/fragment/FragmentDecode.java
+++ app/src/main/java/com/stegano/strenggeheim/fragment/FragmentDecode.java
@@ -128,8 +128,8 @@ public class FragmentDecode extends Fragment {
     }
 
     private void galleryIntent() {
-        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+        galleryIntent.setType("image/*");
         startActivityForResult(galleryIntent, GALLERY);
     }
 
--- app/src/main/java/com/stegano/strenggeheim/fragment/FragmentEncode.java
+++ app/src/main/java/com/stegano/strenggeheim/fragment/FragmentEncode.java
@@ -57,8 +57,8 @@ public class FragmentEncode extends Fragment {
     }
 
     private void galleryIntent() {
-        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+        galleryIntent.setType("image/*");
         startActivityForResult(galleryIntent, GALLERY);
     }
 
