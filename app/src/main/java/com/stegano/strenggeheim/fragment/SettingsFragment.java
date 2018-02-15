10b1b6224068201b154128265527b159798ee487
Jai Prakash <jaiprak@thoughtworks.com>
2018-02-15 16:00:19 +0530

    Saving preferences on preference change | Jai

--- app/src/main/java/com/stegano/strenggeheim/fragment/SettingsFragment.java
+++ app/src/main/java/com/stegano/strenggeheim/fragment/SettingsFragment.java
@@ -1,14 +1,50 @@
 package com.stegano.strenggeheim.fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 import com.stegano.strenggeheim.R;
 
 public class SettingsFragment extends PreferenceFragment {
+
+    SharedPreferences sharedPref;
+    ListPreference hashingPreference;
+    ListPreference encryptionPreference;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+        sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        PreferenceManager.setDefaultValues(getActivity(), R.xml.preferences, false);
+
+        final SharedPreferences.Editor editor = sharedPref.edit();
+        final String hashingPref = getString(R.string.list_prefs_key_hashing);
+        final String encryptionPref = getString(R.string.list_prefs_key_encryption);
+
+        hashingPreference = (ListPreference) findPreference(hashingPref);
+        encryptionPreference = (ListPreference) findPreference(encryptionPref);
+
+        hashingPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                editor.putString(hashingPref, (String) newValue);
+                return editor.commit();
+            }
+        });
+
+        encryptionPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                editor.putString(encryptionPref, (String) newValue);
+                return editor.commit();
+            }
+        });
     }
+
 }
